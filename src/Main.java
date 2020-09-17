ba420929251e08da1715520fccd505cfae97a826
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-17 08:59:23 +0800

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
+/*
+学习目标：- 栈(抽象数据类型)是元素的集合
+        - 三种操作：
+            - push(压栈/入栈):在集合中添加元素
+            - pop(出栈):删除最新添加的元素（访问栈顶的元素,调用后,栈顶元素从栈中被永久性地删除）
+            - peek:访问栈顶元素，不修改该元素（只返回栈顶元素,而不删除它）
+实际应用：- 实现深度优先搜索算法遍历时，使用栈(如果某基准节点四周不存在活节点，则认为此基准节点为死节点，将其从栈中弹出，返回上次基准节点继续探测)
+        - https://www.jianshu.com/p/d60ac14da76f
+概念定义：后进的元素，先出
+另请参阅：- https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
+调用API: - https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html
+*/
+
 import java.util.*;
 
 public class Main {
